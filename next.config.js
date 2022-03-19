a0c0c1af5df7fa3eba22054a7ec8cc9c62a1ffa1
Quang <buidinhquanghuy0312@gmail.com>
2022-03-20 00:21:57 +0700

    update

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ module.exports = {
   images: {
     domains: ['media0.giphy.com', 'media1.giphy.com', 'media2.giphy.com', 'media3.giphy.com', 'media4.giphy.com']
   },
-  experimental: {
-    concurrentFeatures: true,
-  },
+
 }
 
